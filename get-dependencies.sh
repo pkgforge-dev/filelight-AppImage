9b9b6893c15d8316da5d462931789c98bdd3d256
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-23 20:50:18 -0400

    add `kvantum` and `qt6ct`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm filelight
+pacman -Syu --noconfirm filelight kvantum qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
