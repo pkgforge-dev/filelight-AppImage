603dff8e857167c129b3d8e8405fa873548fc3f3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-08 02:11:43 -0400

    test no hardware accel

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/filelight.png
 export DESKTOP=/usr/share/applications/org.kde.filelight.desktop
+export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/filelight
